2208ea2019a910e3a9525717e556b2f192e2c9c6
Pavel-Shipitsyn <117993255+Pavel-Shipitsyn@users.noreply.github.com>
2022-12-16 10:49:17 +0300

    Add files via upload

---
+++ 4.pas
@@ -0,0 +1,27 @@
+﻿var
+  F_in,F_out: Text;
+  Name,line: string;
+  K,i:integer;
+ 
+begin
+ Write('K: ');
+ Readln(K);
+ Assign(F_in,'c:\text.txt');
+ Assign(F_out,'c:\text1.txt');
+
+ Reset(F_in);
+ Rewrite(F_out);
+ 
+ i:=0;
+ While not eof(F_in) do
+  begin
+   Readln(F_in,line);
+   inc(i);
+   if i<>K then Writeln(F_out,line);
+  end;
+ 
+ Close(F_in);
+ Close(F_out);
+ Erase(F_in);
+ Rename(F_out,'c:\text.txt');
+end.
